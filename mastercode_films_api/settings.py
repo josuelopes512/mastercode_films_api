6076dd386de7fa1d9defc59df4b7962b3e83cb9b
Josué Lopes <josue.s.lopes.512@gmail.com>
2022-03-01 13:21:07 +0000

    default ALLOWED_HOSTS

--- mastercode_films_api/settings.py
+++ mastercode_films_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env.str('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost']))
 
 
 # Application definition
